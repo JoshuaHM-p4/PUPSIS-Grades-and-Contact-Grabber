c2bb03a61198a300f7368a4c7b41c6ed53c3a623
Aidan Tiu <tiuprof77@gmail.com>
2024-05-28 23:38:19 +0800

    Modify and add feature functions

--- utils/gwaCalculator.js
+++ utils/gwaCalculator.js
@@ -1,9 +1,10 @@
+// Calculate the GWA of a student given the grades and units of the subjects
 function calculateGWA(grades, units) {
     let unit_total = 0;
     let grade_total = 0;
 
     for (let i = 0; i < grades.length; i++)  {
-        if (grades[i] !== null || grades[i] !== "") {
+        if (grades[i] !== null && grades[i] !== "") {
             grade_total += parseFloat(grades[i]) * units[i];
             unit_total += units[i];
         }
@@ -14,8 +15,42 @@ function calculateGWA(grades, units) {
     return gwa;
 }
 
-let grades = ["1.00", "1.25"];
-let units = [2, 2]
-let gwa = calculateGWA(grades, units);
+// Determine the standing of a student based on the GWA
+function determineStanding(gwa) {
 
-console.log("GWA: ", gwa)
+    if (1.00 <= gwa && gwa <= 1.50) {
+        return "Deans Lister";
+    } else if (1.51 <= gwa && gwa <= 1.75) {
+        return "President's Lister";
+    } else {
+        return "Good Student"
+    }
+}
+
+// Determine the Latin Honors of a student based on the GWA
+function determineLatinHonors(gwa) {
+    if (1.000 <= gwa && gwa <= 1.1500) {
+        return "Summa Cum Laude";
+    } else if (1.151 <= gwa && gwa <= 1.3500) {
+        return "Magna Cum Laude";
+    } else if (1.351 <= gwa && gwa <= 1.6000) {
+        return "Cum Laude";
+    } else {
+        return "Good Student";
+    }
+}
+
+// Determine the status of a student based on the standing
+function determineStatus(standing) {
+    if (standing === "Deans Lister" || standing === "President's Lister") {
+        return "Honor Student";
+    } else {
+        return "Good Student";
+    }
+}
+
+// Expose functions to be used in other scripts
+window.calculateGWA = calculateGWA;
+window.determineStanding = determineStanding;
+window.determineLatinHonors = determineLatinHonors;
+window.determineStatus = determineStatus;
